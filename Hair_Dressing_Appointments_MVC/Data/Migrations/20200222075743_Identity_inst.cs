ccb80b1b7952014ae1a06f21d35f889a99313b47
harpreet <harpreet76774@gmail.com>
2020-03-03 10:22:48 +1300

    Add project files.

---
+++ Hair_Dressing_Appointments_MVC/Data/Migrations/20200222075743_Identity_inst.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hair_Dressing_Appointments_MVC.Data.Migrations
+{
+    public partial class Identity_inst : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
